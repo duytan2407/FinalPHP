f5a26606d293b5609de0af91d6f50f3290dbfcdb
duytan2407 <nguyendam554@gmail.com>
2022-04-10 05:14:14 +0700

    final user , thieu admin nữa là xong ha , trước khi dùng cài composer

---
+++ resources/views/page/loai_sanpham.blade.php
@@ -0,0 +1,115 @@
+@extends('master')
+@section('content')
+<div class="inner-header">
+		<div class="container">
+			<div class="pull-left">
+				<h6 class="inner-title">{{$dacsanmien->name}}</h6>
+			</div>
+			<div class="pull-right">
+				<div class="beta-breadcrumb font-large">
+					<a href="{{route('trang-chu')}}">Trang Chủ</a> / <span>Sản phẩm</span>
+				</div>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	</div>
+	<div class="container">
+		<div id="content" class="space-top-none">
+			<div class="main-content">
+				<div class="space60">&nbsp;</div>
+				<div class="row">
+					<div class="col-sm-3">
+						<ul class="aside-menu">
+								@foreach($loai as $l)
+						<li><a href="{{route('loaisanpham',$l->id)}}">{{$l->name}}</a></li>
+								@endforeach
+						</ul>
+					</div>
+					<div class="col-sm-9">
+						<div class="beta-products-list">
+							<h4>Ngon Ngon Bổ Dưỡng</h4>
+							<div class="beta-products-details">
+								<p class="pull-left">Tìm thấy {{count($theoloai)}}</p>
+								<div class="clearfix"></div>
+							</div>
+
+							<div class="row">
+								@foreach($theoloai as $sp)
+								<div class="col-sm-4">
+									<div class="single-item">
+										@if($sp->promotion_price!=0)
+										<div class="ribbon-wrapper"><div class="ribbon sale">Sale</div></div>
+										@endif
+										<div class="single-item-header">
+											<a href="{{route('chitietsanpham',$sp->id)}}"><img src="source/image/product/{{$sp->image}}" alt="" height="250px"></a>
+										</div>
+										<div class="single-item-body">
+											<p class="single-item-title">{{$sp->name}}</p>
+											<p class="single-item-price" style="font-size: 18px">
+											@if($sp->promotion_price !=0)
+												<span class="flash-del">{{number_format($sp->unit_price)}} đồng</span>
+												<span class="flash-sale">{{number_format($sp->promotion_price)}} đồng</span>
+											@else
+												<span>{{number_format($sp->unit_price)}} đồng</span>
+											@endif
+										</div>
+										<div class="single-item-caption">
+											<a class="add-to-cart pull-left" href="shopping_cart.html"><i class="fa fa-shopping-cart"></i></a>
+											<a class="beta-btn primary" href="product.html">Details <i class="fa fa-chevron-right"></i></a>
+											<div class="clearfix"></div>
+										</div>
+									</div>
+								</div>
+									@endforeach
+							</div>
+						</div> <!-- .beta-products-list -->
+
+						<div class="space50">&nbsp;</div>
+
+						<div class="beta-products-list">
+							<h4>Woa Woa Woa</h4>
+							<div class="beta-products-details">
+								<p class="pull-left">Tìm thấy {{count($sp_khac)}}</p>
+								<div class="clearfix"></div>
+							</div>
+							<div class="row">
+								@foreach($sp_khac as $khac)
+								<div class="col-sm-4">
+									<div class="single-item">
+										@if($sp->promotion_price!=0)
+										<div class="ribbon-wrapper"><div class="ribbon sale">Sale</div></div>
+										@endif
+										<div class="single-item-header">
+											<a href="{{route('chitietsanpham',$khac->id)}}"><img src="source/image/product/{{$khac->image}}" alt="" height="250px"></a>
+										</div>
+										<div class="single-item-body">
+											<p class="single-item-title">{{$khac->name}}</p>
+											<p class="single-item-price" style="font-size: 18px">
+											@if($sp->promotion_price !=0)
+												<span class="flash-del">{{number_format($khac->unit_price)}} đồng</span>
+												<span class="flash-sale">{{number_format($khac->promotion_price)}} đồng</span>
+											@else
+												<span>{{number_format($khac->unit_price)}} đồng</span>
+											@endif
+										</div>
+										<div class="single-item-caption">
+											<a class="add-to-cart pull-left" href="shopping_cart.html"><i class="fa fa-shopping-cart"></i></a>
+											<a class="beta-btn primary" href="product.html">Details <i class="fa fa-chevron-right"></i></a>
+											<div class="clearfix"></div>
+										</div>
+									</div>
+								</div>
+									@endforeach
+								
+							</div>
+							<div class="space40">&nbsp;</div>
+							
+						</div> <!-- .beta-products-list -->
+					</div>
+				</div> <!-- end section with sidebar and main content -->
+
+
+			</div> <!-- .main-content -->
+		</div> <!-- #content -->
+	</div> <!-- .container -->
+@endsection
